ade2d3b185d80241ea7de0a504ebe8b17793e0eb
Grant Zeng <grant.zeng@student.unsw.edu.au>
2023-12-20 20:30:36 +1100

    stub dynamic array

---
+++ data_structures/python/dynamic_array.py
@@ -0,0 +1,22 @@
+"""
+    Idea: avoid paying O(n) for insert by overallocating memory
+
+    Implements sequence interface
+
+    (Yes, I know this looks ridiculous in Python.)
+
+    TODO: Implement later
+"""
+class Array():
+    def __init__(self):
+        pass
+
+
+class Dynamic_Array(Array):
+
+    def __init__(self, ratio):
+        pass
+
+    def __len__(self):
+        pass
+
