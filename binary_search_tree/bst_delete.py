e2564635155c160925fa9f152e5858dab0ec22aa
Grant Zeng <grant.zeng@student.unsw.edu.au>
2024-01-29 11:39:14 +1100

    debugging bst delete

--- binary_search_tree/bst_delete.py
+++ binary_search_tree/bst_delete.py
@@ -48,22 +48,54 @@ class BSTree:
 
 
 
-    def delete(self, n):
-        """ Delete node n in the tree """
-        if not n.left:              self._transplant(n, n.right)
-        elif not n.right:           self._transplant(n, n.left)
-        else:
+    # def delete_incorrect(self, n):
+    #     """ Delete node n in the tree """
+    #     if not n.left:              self._transplant(n, n.right)
+    #     elif not n.right:           self._transplant(n, n.left)
+    #     else:
 
+    #         s = n.successor()
+
+    #         if s.right: # I'm not 100% sure this is correct?
+    #             # This doens't work:
+    #             # - try the example where the successor is n.right
+    #             #   the pointers go everywhere and it's a bloody mess
+    #             self._transplant(s, s.right)
+    #             s.right = n.right
+    #             s.right.parent = s
+
+    #         self._transplant(n, s)
+    #         s.left = n.left
+    #         s.left.parent = s
+
+    def delete(self, n):
+        if not n.left:
+            self._transplant(n, n.right)
+        elif not n.right:
+            self._transplant(n, n.left)
+        else:
             s = n.successor()
 
-            if s.right: # I'm not 100% sure this is correct?
+            if n.right is not s:
+                # successor is further down tree,
+                # - need to extract it from its location and replace with its right child
+                # - successor can't have left child (else that would be the successor!)
                 self._transplant(s, s.right)
-                s.right = n.right
+                s.right = n.right # Attach n's right children to s
                 s.right.parent = s
 
             self._transplant(n, s)
-            s.left = n.left
+            s.left = n.left  # Attach n's left children to n
             s.left.parent = s
+
+        # n is now free
+        # - if this was C, this would cause a memory leak
+
+    # TODO
+    # - Implement alternative bst_delete using 'tree_join'as a primitive
+    # - Implement recursive bst_delete
+    # - Implement bst_delete where you do data swaps to a leaf node, and delete the leaf instead of all this pointer stuff
+
 
 
 
